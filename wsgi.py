12aa29dc8e02e201eaad7bb1897f40878be3c220
Danilo <danilorodrigues128@hotmail.com>
2022-03-19 18:23:25 +0100

    Codigo Andreis 2

--- wsgi.py
+++ wsgi.py
@@ -9,7 +9,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 
-app = Flask(_name_)
+app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
